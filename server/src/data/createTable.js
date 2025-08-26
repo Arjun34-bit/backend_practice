b06286b3b7b65da9f6ce60d59935f487a33463a6
Arjun Madhu Devendra <ARJUNDEVENDRA34@GMAIL.COM>
2025-08-26 21:22:51 +0530

    first commit

---
+++ server/src/data/createTable.js
@@ -0,0 +1,43 @@
+import pool from "../config/db.js";
+
+const createUserTable = async () => {
+  const queryText = `CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    role VARCHAR(50) DEFAULT 'sales_rep',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);`;
+
+  try {
+    pool.query(queryText);
+    console.log("user table created");
+  } catch (error) {
+    console.log("Error User employees table:", error);
+  }
+};
+
+const createLeadTable = async () => {
+  const queryText = `CREATE TABLE IF NOT EXISTS leads (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone VARCHAR(15),
+    status VARCHAR(50) DEFAULT 'new', 
+    source VARCHAR(100),               
+    assigned_to INT REFERENCES users(id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);`;
+
+  try {
+    pool.query(queryText);
+    console.log("Lead table created");
+  } catch (error) {
+    console.log("Error creating Lead table:", error);
+  }
+};
+
+export default { createUserTable, createLeadTable };
---
+++ server/src/data/data.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    role VARCHAR(50) DEFAULT 'sales_rep',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS leads (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone VARCHAR(15),
+    status VARCHAR(50) DEFAULT 'new',  -- new, contacted, qualified, converted, unqualified
+    source VARCHAR(100),               -- referral, campaign, website
+    assigned_to INT REFERENCES users(id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
